4388b6e48e64a3ae87bf3eaaaaf55ea781c7eef5
Dmitry Krayushkin <krayushkin.dm@gmail.com>
2024-05-31 17:12:11 +0400

    add auto open book after downloading; info implemented

--- frontend/apps/cloudstorage/telegram.lua
+++ frontend/apps/cloudstorage/telegram.lua
@@ -10,14 +10,11 @@ local util = require("util")
 local socket = require("socket")
 local socketutil = require("socketutil")
 local http = require("socket.http")
-
+local logger = require("logger")
 local T = require("ffi/util").template
 local _ = require("gettext")
 
 local Telegram = {offset = 0}
-
-
-
 
 local function concatTableKeys(t)
     local keys = {}
@@ -68,10 +65,11 @@ end
 function Telegram:getFileUrl(file_id, token)
     TelegramApi.token = token
     success = TelegramApi.get_file(file_id)
-    if type(success) == "table" and type(success.result) == "table" then
+    if type(success) == "table" and type(success.result) == "table" and success.result.file_path then
         file_path = success.result.file_path
         return "https://api.telegram.org/file/bot" .. token .. "/" .. file_path
     else
+        logger.warn("Telegram.getFileUrl: get_file error")
         return false
     end
 end
@@ -85,12 +83,10 @@ function downloadFileFromUrl(url, local_path)
     })
     socketutil:reset_timeout()
     if code ~= 200 then
-        logger.warn("Telegram cannot download file:", status or code)
+        logger.warn("Telegram: can't download file:", status or code)
     end
     return code
 end
-
-
 
 
 function Telegram:downloadFile(item, address, username, password, path, callback_close)
@@ -106,17 +102,38 @@ function Telegram:downloadFile(item, address, username, password, path, callback
     if url then
         local code = downloadFileFromUrl(url, path)
         if code == 200 then
+            local __, filename = util.splitFilePathName(path)
+            if G_reader_settings:isTrue("show_unsupported") and not DocumentRegistry:hasProvider(filename) then
                 UIManager:show(InfoMessage:new{
-                    text = T(_("File saved!\n")),
+                    text = T(_("File saved to:\n%1"), BD.filename(path)),
                 })
+            else
+                UIManager:show(ConfirmBox:new{
+                    text = T(_("File saved to:\n%1\nWould you like to read the downloaded book now?"),
+                        BD.filepath(path)),
+                    ok_callback = function()
+                        local Event = require("ui/event")
+                        UIManager:broadcastEvent(Event:new("SetupShowReader"))
+
+                        if callback_close then
+                            callback_close()
+                        end
+
+                        ReaderUI:showReader(path)
+                    end
+                })
+            end
         else
             UIManager:show(InfoMessage:new{
-                text = T(_("Could not save file.\n")),
+                text = T(_("Could not save file to:\n%1"), BD.filepath(path)),
                 timeout = 3,
             })
         end
     else
-        print("can't get valid url")
+        UIManager:show(InfoMessage:new{
+            text = T(_("Could not save file to:\n%1"), BD.filepath(path)),
+            timeout = 3,
+        })
     end
 end
 
@@ -125,7 +142,7 @@ end
 function Telegram:config(item, callback)
     local text_info = _([[
 For using this functionality you must first create bot in Telegram client application using @BotFather.
-Supply provided token in token field. Send books to this device just post to bot
+Paste provided token in token field. Send books to this device just post to bot
 book file or http/https link. Select books from list and download it (max size <= 20 MB).]])
     local text_name, text_token, text_appkey, text_url
     if item then
@@ -180,10 +197,19 @@ book file or http/https link. Select books from list and download it (max size <
     self.settings_dialog:onShowKeyboard()
 end
 
-function Telegram:info(token)
+function Telegram:info(item)
+    local yes_no = function(v) return v and _("Yes") or _("No")  end
+    TelegramApi.token = item.password
+    local success, res = TelegramApi.get_me()
+    if type(success) == "table" and type(success.result) == "table" then
+        local info = success.result
         UIManager:show(InfoMessage:new{
-            text = T(_"Nothing to say here"),
+            text = T(_"Username: %1\nFirst name: %2\nCan join groups: %3\nCan read all group messages: %4",
+                info.username, info.first_name,
+                yes_no(info.can_join_groups),
+                yes_no(info.can_read_all_group_messages))
         })
+    end
 end
 
 return Telegram
